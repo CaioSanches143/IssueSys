9926f5f10e25c0915494345395c2dc4d6a2e02f3
Caio Sanches <1210092@isep.ipp.pt>
2022-01-28 11:02:33 +0000

    [DEVELOPMENT][ITERATION-1][BACKEND] creating project in spring with some dependecies like lombok, JPA, Docs

---
+++ Project/src/main/java/uff/issuesys/IssueSysApplication.java
@@ -0,0 +1,13 @@
+package uff.issuesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueSysApplication.class, args);
+    }
+
+}
---
+++ Project/src/test/java/uff/issuesys/IssueSysApplicationTests.java
@@ -0,0 +1,13 @@
+package uff.issuesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
